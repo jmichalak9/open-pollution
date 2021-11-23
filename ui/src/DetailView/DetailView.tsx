d6f752f25cb71ebcf317ab8f61173239e367ecfa
Jakub Michalak <michalakjakub1999@gmail.com>
2021-11-23 09:47:30 +0100

    Add styling to ui components

--- ui/src/DetailView/DetailView.tsx
+++ ui/src/DetailView/DetailView.tsx
@@ -14,21 +14,26 @@ const DetailView: React.FC<DetailViewProps> = ({ mapData }: DetailViewProps) => 
   const long = mapData.measurement.position.long;
 
   return (
-    <div className="DetailView">
-      <h1>Szczegóły pomiaru</h1>
-      <div className="LeftDetails">
-        <div>Szerokość geograficzna {lat}°{lat > 0 ? <text>N</text> : <text>S</text>}</div>
-        <div>Długość geograficzna {long}°{long > 0 ? <text>E</text> : <text>W</text>}</div>
-        { mapData.measurement.temperature ? <div>Temperatura {mapData.measurement.temperature}°C</div> : <div></div>}
-      </div>
-      <div className="RightDetails">
-        { mapData.measurement.levelPM10 ? <div>Zanieczyszczenie PM 10 {mapData.measurement.levelPM10} μg/m<sup>3</sup></div> : <div></div>}
-        { mapData.measurement.levelPM25 ? <div>Zanieczyszczenie PM 2.5 {mapData.measurement.levelPM25} μg/m<sup>3</sup></div> : <div></div>}
-        { mapData.measurement.levelSO2 ? <div>Zanieczyszczenie SO<sub>2</sub> {mapData.measurement.levelSO2} μg/m<sup>3</sup></div> : <div></div>}
-        { mapData.measurement.levelO3 ? <div>Zanieczyszczenie O<sub>3</sub> {mapData.measurement.levelO3} μg/m<sup>3</sup></div> : <div></div>}
-      </div>
+  <div className="DetailView">
+    <div className="DetailsFlexPanel">
+
+      <div>Szerokość geograficzna</div>
+      <div>{lat}°{lat > 0 ? <text>N</text> : <text>S</text>}</div>
+
+      <div>Długość geograficzna</div>
+      <div>{long}°{long > 0 ? <text>E</text> : <text>W</text>}</div>
+
+      { mapData.measurement.temperature ? <div className="DetailsFlexPanel"><div>Temperatura</div> <div>{mapData.measurement.temperature}°C</div></div> : <div></div>}
     </div>
-  );
+    <div>
+      { mapData.measurement.levelPM10 ? <div className="DetailsFlexPanel"><div>Zanieczyszczenie PM 10</div> <div>{mapData.measurement.levelPM10} μg/m<sup>3</sup></div></div> : <div></div>}
+      { mapData.measurement.levelPM25 ? <div className="DetailsFlexPanel"><div>Zanieczyszczenie PM 2.5</div> <div>{mapData.measurement.levelPM25} μg/m<sup>3</sup></div></div> : <div></div> }
+      { mapData.measurement.levelSO2 ? <div className="DetailsFlexPanel"><div>Zanieczyszczenie SO<sub>2</sub></div> <div>{mapData.measurement.levelSO2} μg/m<sup>3</sup></div></div> : <div></div>}
+      { mapData.measurement.levelO3 ? <div className="DetailsFlexPanel"><div>Zanieczyszczenie O<sub>3</sub></div> <div>{mapData.measurement.levelO3} μg/m<sup>3</sup></div></div>: <div></div>}
+    </div>
+  </div>
+
+);
 }
 
 export default DetailView;
