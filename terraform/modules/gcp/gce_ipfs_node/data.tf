0e481bb2472a8f3412458386a7839918cdf4490b
Arkadiusz Noster <x-arkadiusz.noster@upvest.co>
2022-01-30 17:18:04 +0100

    chore(terraform): cleanup and fix for IPFS node

--- terraform/modules/gcp/gce_ipfs_node/data.tf
+++
@@ -1,9 +0,0 @@
-data "google_compute_subnetwork" "subnet" {
-  self_link = var.subnetwork
-}
-
-locals {
-  project = data.google_compute_subnetwork.subnet.project
-  network = data.google_compute_subnetwork.subnet.network
-  region = data.google_compute_subnetwork.subnet.region
-}
--- terraform/modules/gcp/gce_sentinel/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "sentinel-instance" {
-  value = google_compute_instance.acceptance-sentinel-vm
-}
-
-output "sentinel-public-ip" {
-  value = google_compute_address.external-sentinel-address.address
-}
