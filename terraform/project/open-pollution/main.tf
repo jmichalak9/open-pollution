a2a268cbda02f7e0d16332f749bdd8fe9a3260d6
Jakub Michalak <michalakjakub1999@gmail.com>
2022-01-30 16:48:41 +0100

    Add missing tf modules

--- terraform/project/open-pollution/main.tf
+++ terraform/project/open-pollution/main.tf
@@ -19,11 +19,11 @@ module "ipfs-node" {
 
   depends_on = [null_resource.add-ipfs-image]
 }
-#
-#module "sentinel" {
-#  source = "../../modules/gcp/gce_sentinel"
-#
-#  ipfs-node-self-link = module.ipfs-node.ipfs-node-instance.self_link
-#  registry_bucket_id  = google_container_registry.registry.id
-#  sentinel_image      = "eu.gcr.io/${local.project}/${local.sentinel-image-name}:latest"
-#}
+
+module "sentinel" {
+  source = "../../modules/gcp/gce_sentinel"
+
+  ipfs-node-self-link = module.ipfs-node.ipfs-node-instance.self_link
+  registry_bucket_id  = google_container_registry.registry.id
+  sentinel_image      = "eu.gcr.io/${local.project}/${local.sentinel-image-name}:latest"
+}
